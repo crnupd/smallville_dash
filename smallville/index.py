fe56aab04bcf9e8a846a57060ad25e82f7abc608
Chelsey0626 <cmrogador@gmail.com>
2024-12-16 23:27:23 +0800

    update index.py (added line for importing ADMINUSERID to payment page)

--- smallville/index.py
+++ smallville/index.py
@@ -14,131 +14,132 @@ from apps.schedule import student_sched, sched_assign, sched_edit, sched_form, s
 from apps.payment import payment, payment_upload
 from apps.teacher import teacher_stud_list, teacher_stud_list_edit
 
-ADMIN_USER_ID = 5
-
-# Define the main layout of the app
-app.layout = html.Div(
-    [
-        dcc.Location(id='url', refresh=True),  # URL location for navigation
-       
-        # LOGIN DATA
-        # 1) logout indicator, storage_type='session' means that data will be retained
-        #  until browser/tab is closed (vs clearing data upon refresh)
-        dcc.Store(id='sessionlogout', data=True, storage_type='session'),
-
-        # 2) current_user_id -- stores user_id
-        dcc.Store(id='currentuserid', data=-1, storage_type='session'),
-       
-        # 3) currentrole -- stores the role
-        # we will not use them but if you have roles, you can use it
-        dcc.Store(id='currentrole', data=-1, storage_type='session'),
-
-        html.Div(cm.navbar, id='navbar_div'),  
-
-        # Page Content
-        html.Div(style={'margin-top': '70px'},id='page-content', className='m-2 p-2')
-    ]
-)
-
-@app.callback(
-    [
-        Output('page-content', 'children'),
-        Output('sessionlogout', 'data'),
-        Output('navbar_div', 'className'),
-    ],
-    [
-        # If the path (i.e. part after the website name;
-        # in url = youtube.com/watch, path = '/watch') changes,
-        # the callback is triggered
-        Input('url', 'pathname')
-    ],
-    [
-        State('sessionlogout', 'data'),
-        State('currentuserid', 'data'),
-    ]
-)
-
-
-def displaypage (pathname, sessionlogout, userid):
-    ctx = dash.callback_context
-    if ctx.triggered:
-        eventid = ctx.triggered[0]['prop_id'].split('.')[0]
-        if eventid == 'url':
-            if userid < 0: # if logged out
-                if pathname == '/' or pathname == '/login':
-                    returnlayout = login.layout
-                elif pathname == '/signup':
-                    returnlayout = signup.layout
-                else:
-                    returnlayout = '404: request not found before login'
-           
-            else:    
-                if pathname == '/logout':
-                    returnlayout = login.layout, True, ''
-                   
-                elif pathname == '/home':
-                    #return student_profile.layout  # Assuming you have this layout defined
-                    return home.layout, sessionlogout, ''
-                
-                elif pathname == '/admin':
-                    if userid == ADMIN_USER_ID:  # Check if logged-in user is admin
-                        return admin.layout, sessionlogout, ''
-                    else:
-                        returnlayout = '403: Access Denied - Admins Only'
-                    ##currently still not working, your thoughts pls (working yung part na access denied, natatakpan lang ng navbar haha funny)
-               
-                elif pathname == '/student/student_profile':
-                    #return student_profile.layout  # Assuming you have this layout defined
-                    return student_profile.layout, sessionlogout, ''
-       
-                elif pathname == '/student/student_profile_edit':
-                    return student_profile_edit.layout, sessionlogout, ''
-       
-                elif pathname == '/student/student_sched':
-                    return student_sched.layout, sessionlogout, ''
-
-                elif pathname == '/student/payment':
-                    return payment.layout, sessionlogout, ''
-
-                elif pathname == '/student/payment_upload':
-                    return payment_upload.layout, sessionlogout, ''
-       
-                elif pathname == '/student/sched_management':
-                    return sched_management.layout, sessionlogout, ''
-       
-                elif pathname == '/student/sched_form':
-                    return sched_form.layout, sessionlogout, ''
-       
-                elif pathname == '/student/sched_edit':
-                    return sched_edit.layout, sessionlogout, ''
-                
-                elif pathname == '/teacher/teacher_stud_list':
-                    return teacher_stud_list.layout, sessionlogout, ''
-                
-                elif pathname == '/teacher/teacher_stud_list_edit':
-                    return teacher_stud_list_edit, sessionlogout, ''
-
-                else:
-                    returnlayout = '404: request not found after login'
-                   
-            # decide sessionlogout value
-            logout_conditions = [
-                pathname in ['/', '/logout'],
-                userid == -1,
-                not userid
-            ]
-            sessionlogout = any(logout_conditions)
-           
-            # hide navbar if logged-out; else, set class/style to default
-            navbar_classname = 'd-none' if sessionlogout else ''
-       
-        else:
-            raise PreventUpdate
-   
-        return [returnlayout, sessionlogout, navbar_classname]
-    else:
-        raise PreventUpdate
+ADMIN_USER_ID = 1
 
 if __name__ == '__main__':
-    webbrowser.open('http://127.0.0.1:8050/', new=0, autoraise=True)
-    app.run_server(debug=True)  # Set debug=True for development
+    # Define the main layout of the app
+    app.layout = html.Div(
+        [
+            dcc.Location(id='url', refresh=True),  # URL location for navigation
+        
+            # LOGIN DATA
+            # 1) logout indicator, storage_type='session' means that data will be retained
+            #  until browser/tab is closed (vs clearing data upon refresh)
+            dcc.Store(id='sessionlogout', data=True, storage_type='session'),
+
+            # 2) current_user_id -- stores user_id
+            dcc.Store(id='currentuserid', data=-1, storage_type='session'),
+        
+            # 3) currentrole -- stores the role
+            # we will not use them but if you have roles, you can use it
+            dcc.Store(id='currentrole', data=-1, storage_type='session'),
+
+            html.Div(cm.navbar, id='navbar_div'),  
+
+            # Page Content
+            html.Div(style={'margin-top': '70px'},id='page-content', className='m-2 p-2')
+        ]
+    )
+
+    @app.callback(
+        [
+            Output('page-content', 'children'),
+            Output('sessionlogout', 'data'),
+            Output('navbar_div', 'className'),
+        ],
+        [
+            # If the path (i.e. part after the website name;
+            # in url = youtube.com/watch, path = '/watch') changes,
+            # the callback is triggered
+            Input('url', 'pathname')
+        ],
+        [
+            State('sessionlogout', 'data'),
+            State('currentuserid', 'data'),
+        ]
+    )
+
+
+    def displaypage (pathname, sessionlogout, userid):
+        ctx = dash.callback_context
+        if ctx.triggered:
+            eventid = ctx.triggered[0]['prop_id'].split('.')[0]
+            if eventid == 'url':
+                if userid < 0: # if logged out
+                    if pathname == '/' or pathname == '/login':
+                        returnlayout = login.layout
+                    elif pathname == '/signup':
+                        returnlayout = signup.layout
+                    else:
+                        returnlayout = '404: request not found before login'
+            
+                else:    
+                    if pathname == '/logout':
+                        returnlayout = login.layout, True, ''
+                    
+                    elif pathname == '/home':
+                        #return student_profile.layout  # Assuming you have this layout defined
+                        return home.layout, sessionlogout, ''
+                    
+                    elif pathname == '/admin':
+                        if userid == ADMIN_USER_ID:  # Check if logged-in user is admin
+                            return admin.layout, sessionlogout, ''
+                        else:
+                            returnlayout = '403: Access Denied - Admins Only'
+                        ##currently still not working, your thoughts pls (working yung part na access denied, natatakpan lang ng navbar haha funny)
+                
+                    elif pathname == '/student/student_profile':
+                        #return student_profile.layout  # Assuming you have this layout defined
+                        return student_profile.layout, sessionlogout, ''
+        
+                    elif pathname == '/student/student_profile_edit':
+                        return student_profile_edit.layout, sessionlogout, ''
+        
+                    elif pathname == '/student/student_sched':
+                        return student_sched.layout, sessionlogout, ''
+
+                    elif pathname == '/student/payment':
+                        return payment.layout, sessionlogout, ''
+
+                    elif pathname == '/student/payment_upload':
+                        return payment_upload.layout, sessionlogout, ''
+        
+                    elif pathname == '/student/sched_management':
+                        return sched_management.layout, sessionlogout, ''
+        
+                    elif pathname == '/student/sched_form':
+                        return sched_form.layout, sessionlogout, ''
+        
+                    elif pathname == '/student/sched_edit':
+                        return sched_edit.layout, sessionlogout, ''
+                    
+                    elif pathname == '/teacher/teacher_stud_list':
+                        return teacher_stud_list.layout, sessionlogout, ''
+                    
+                    elif pathname == '/teacher/teacher_stud_list_edit':
+                        return teacher_stud_list_edit, sessionlogout, ''
+
+                    else:
+                        returnlayout = '404: request not found after login'
+                    
+                # decide sessionlogout value
+                logout_conditions = [
+                    pathname in ['/', '/logout'],
+                    userid == -1,
+                    not userid
+                ]
+                sessionlogout = any(logout_conditions)
+            
+                # hide navbar if logged-out; else, set class/style to default
+                navbar_classname = 'd-none' if sessionlogout else ''
+        
+            else:
+                raise PreventUpdate
+    
+            return [returnlayout, sessionlogout, navbar_classname]
+        else:
+            raise PreventUpdate
+
+    if __name__ == '__main__':
+        webbrowser.open('http://127.0.0.1:8050/', new=0, autoraise=True)
+        app.run_server(debug=True)  # Set debug=True for development
