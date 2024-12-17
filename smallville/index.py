df3ece2dbe3aa0a5676460c78c3a500b5f1118ea
nicolejasmine98 <nrgonzales@up.edu.ph>
2024-12-18 00:04:41 +0800

    index edits on main stuff

--- smallville/index.py
+++ smallville/index.py
@@ -14,125 +14,109 @@ from apps.payment import payment, payment_upload
 from apps.teacher import teacher_stud_list_edit
 from constants import ADMIN_USER_ID
 
+if __name__ == '__main__':
 
-# Define the main layout of the app
-app.layout = html.Div(
-    [
-        dcc.Location(id='url', refresh=True),  # URL location for navigation
-    
-        # LOGIN DATA
-        # 1) logout indicator, storage_type='session' means that data will be retained
-        #  until browser/tab is closed (vs clearing data upon refresh)
-        dcc.Store(id='sessionlogout', data=True, storage_type='session'),
-
-        # 2) current_user_id -- stores user_id
-        dcc.Store(id='currentuserid', data=-1, storage_type='session'),
-    
-        # 3) currentrole -- stores the role
-        # we will not use them but if you have roles, you can use it
-        dcc.Store(id='currentrole', data=-1, storage_type='session'),
-
-        html.Div(cm.navbar, id='navbar_div'),  
-
-        # Page Content
-        html.Div(style={'margin-top': '70px'},id='page-content', className='m-2 p-2')
-    ]
-)
-
-@app.callback(
-    [
-        Output('page-content', 'children'),
-        Output('sessionlogout', 'data'),
-        Output('navbar_div', 'className'),
-    ],
-    [
-        # If the path (i.e. part after the website name;
-        # in url = youtube.com/watch, path = '/watch') changes,
-        # the callback is triggered
-        Input('url', 'pathname')
-    ],
-    [
-        State('sessionlogout', 'data'),
-        State('currentuserid', 'data'),
-    ]
-)
-def displaypage (pathname, sessionlogout, userid):
-    ctx = dash.callback_context
-    if ctx.triggered:
-        eventid = ctx.triggered[0]['prop_id'].split('.')[0]
-        if eventid == 'url':
-            if userid < 0: # if logged out
-                if pathname == '/' or pathname == '/login':
-                    returnlayout = login.layout
-                elif pathname == '/signup':
-                    returnlayout = signup.layout
-                else:
-                    returnlayout = '404: request not found before login'
+    app.layout = html.Div(
+        [
+            dcc.Location(id='url', refresh=True),  
         
-            else:    
-                if pathname == '/logout':
-                    print("path changed to /logout")
-                    returnlayout = login.layout, True, ''
-                
-                elif pathname == '/home':
-                    #return student_profile.layout  # Assuming you have this layout defined
-                    returnlayout = home.layout
-                
-                elif pathname == '/admin':
-                    if userid == ADMIN_USER_ID:  # Check if logged-in user is admin
-                        returnlayout = admin.layout
+            dcc.Store(id='sessionlogout', data=True, storage_type='session'),
+
+            dcc.Store(id='currentuserid', data=-1, storage_type='session'),
+        
+            dcc.Store(id='currentrole', data=-1, storage_type='session'),
+
+            html.Div(cm.navbar, id='navbar_div'),  
+
+            html.Div(style={'margin-top': '70px'},id='page-content', className='m-2 p-2')
+        ]
+    )
+
+    @app.callback(
+        [
+            Output('page-content', 'children'),
+            Output('sessionlogout', 'data'),
+            Output('navbar_div', 'className'),
+        ],
+        [
+            Input('url', 'pathname')
+        ],
+        [
+            State('sessionlogout', 'data'),
+            State('currentuserid', 'data'),
+        ]
+    )
+    def displaypage (pathname, sessionlogout, userid):
+        ctx = dash.callback_context
+        if ctx.triggered:
+            eventid = ctx.triggered[0]['prop_id'].split('.')[0]
+            if eventid == 'url':
+                if userid < 0: # if logged out
+                    if pathname == '/' or pathname == '/login':
+                        returnlayout = login.layout
+                    elif pathname == '/signup':
+                        returnlayout = signup.layout
                     else:
-                        returnlayout = '403: Access Denied - Admins Only'
-                    ##currently still not working, your thoughts pls (working yung part na access denied, natatakpan lang ng navbar haha funny)
+                        returnlayout = '404: request not found before login'
             
-                elif pathname == '/student/student_profile':
-                    #return student_profile.layout  # Assuming you have this layout defined
-                    returnlayout = student_profile.layout
-    
-                elif pathname == '/student/student_profile_edit':
-                    returnlayout = student_profile_edit.layout
-    
-                elif pathname == '/student/student_sched':
-                    returnlayout = student_sched.layout
-
-                elif pathname == '/student/payment':
-                    returnlayout = payment.layout
-
-                elif pathname == '/student/payment_upload':
-                    returnlayout = payment_upload.layout
-    
-                elif pathname == '/student/sched_assign':
-                    returnlayout = sched_assign.layout
-    
-                elif pathname == '/student/sched_form':
-                    returnlayout = sched_form.layout 
-    
-                elif pathname == '/student/sched_edit':
-                    returnlayout = sched_edit.layout
+                else:    
+                    if pathname == '/logout':
+                        print("path changed to /logout")
+                        returnlayout = login.layout, True, ''
+                    
+                    elif pathname == '/home':
+                        returnlayout = home.layout
+                    
+                    elif pathname == '/admin':
+                        if userid == ADMIN_USER_ID: 
+                            returnlayout = admin.layout
+                        else:
+                            returnlayout = '403: Access Denied - Admins Only'
                 
-                elif pathname == '/teacher/teacher_stud_list_edit':
-                    returnlayout = teacher_stud_list_edit.layout
+                    elif pathname == '/student/student_profile':
+                        returnlayout = student_profile.layout
+        
+                    elif pathname == '/student/student_profile_edit':
+                        returnlayout = student_profile_edit.layout
+        
+                    elif pathname == '/student/student_sched':
+                        returnlayout = student_sched.layout
 
-                else:
-                    returnlayout = '404: request not found after login'
-                
-            # decide sessionlogout value
-            logout_conditions = [
-                pathname in ['/', '/logout'],
-                userid == -1,
-                not userid
-            ]
-            sessionlogout = any(logout_conditions)
-            print(sessionlogout)
-            print('--')
-            # hide navbar if logged-out; else, set class/style to default
-            navbar_classname = 'd-none' if sessionlogout else ''
-    
+                    elif pathname == '/student/payment':
+                        returnlayout = payment.layout
+
+                    elif pathname == '/student/payment_upload':
+                        returnlayout = payment_upload.layout
+        
+                    elif pathname == '/student/sched_assign':
+                        returnlayout = sched_assign.layout
+        
+                    elif pathname == '/student/sched_form':
+                        returnlayout = sched_form.layout 
+        
+                    elif pathname == '/student/sched_edit':
+                        returnlayout = sched_edit.layout
+                    
+                    elif pathname == '/teacher/teacher_stud_list_edit':
+                        returnlayout = teacher_stud_list_edit.layout
+
+                    else:
+                        returnlayout = '404: request not found after login'
+                    
+                # decide sessionlogout value
+                logout_conditions = [
+                    pathname in ['/', '/logout'],
+                    userid == -1,
+                    not userid
+                ]
+                sessionlogout = any(logout_conditions)
+                navbar_classname = 'd-none' if sessionlogout else ''
+        
+            else:
+                raise PreventUpdate
+            
+            return [returnlayout, sessionlogout, navbar_classname]
         else:
             raise PreventUpdate
-        
-        return [returnlayout, sessionlogout, navbar_classname]
-    else:
-        raise PreventUpdate
 
-app.run_server(debug=True)  # Set debug=True for development
+    app.run_server(debug=True) 
