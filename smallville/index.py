2020750162c5386309ebd17840a83233422772d4
nicolejasmine98 <nrgonzales@up.edu.ph>
2024-12-17 23:48:38 +0800

    edited index py for layout

--- smallville/index.py
+++ smallville/index.py
@@ -5,10 +5,9 @@ from dash import dcc, html
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
-# Importing your app variable from app.py so we can use it
 from app import app
-from apps import commonmodules as cm  # Assuming you have a common module for navbar etc.
-from apps import home, login, signup, admin  # Import the home layout
+from apps import commonmodules as cm  
+from apps import home, login, signup, admin  
 from apps.student import student_profile, student_profile_edit
 from apps.schedule import student_sched, sched_assign, sched_edit, sched_form, sched_management
 from apps.payment import payment, payment_upload
@@ -16,26 +15,18 @@ from apps.teacher import teacher_stud_list, teacher_stud_list_edit
 from constants import ADMIN_USER_ID
 
 if __name__ == '__main__':
-    # Define the main layout of the app
     app.layout = html.Div(
         [
-            dcc.Location(id='url', refresh=True),  # URL location for navigation
+            dcc.Location(id='url', refresh=True), 
         
-            # LOGIN DATA
-            # 1) logout indicator, storage_type='session' means that data will be retained
-            #  until browser/tab is closed (vs clearing data upon refresh)
             dcc.Store(id='sessionlogout', data=True, storage_type='session'),
 
-            # 2) current_user_id -- stores user_id
             dcc.Store(id='currentuserid', data=-1, storage_type='session'),
-        
-            # 3) currentrole -- stores the role
-            # we will not use them but if you have roles, you can use it
+    
             dcc.Store(id='currentrole', data=-1, storage_type='session'),
 
             html.Div(cm.navbar, id='navbar_div'),  
 
-            # Page Content
             html.Div(style={'margin-top': '70px'},id='page-content', className='m-2 p-2')
         ]
     )
@@ -47,9 +38,6 @@ if __name__ == '__main__':
             Output('navbar_div', 'className'),
         ],
         [
-            # If the path (i.e. part after the website name;
-            # in url = youtube.com/watch, path = '/watch') changes,
-            # the callback is triggered
             Input('url', 'pathname')
         ],
         [
@@ -76,18 +64,18 @@ if __name__ == '__main__':
                         returnlayout = login.layout, True, ''
                     
                     elif pathname == '/home':
-                        #return student_profile.layout  # Assuming you have this layout defined
                         returnlayout = home.layout
                     
                     elif pathname == '/admin':
                         if userid == ADMIN_USER_ID:  # Check if logged-in user is admin
                             returnlayout = admin.layout
                         else:
-                            returnlayout = '403: Access Denied - Admins Only'
-                        ##currently still not working, your thoughts pls (working yung part na access denied, natatakpan lang ng navbar haha funny)
+                            returnlayout = html.Div(
+                            [
+                                html.H1("403: Access Denied - Admins Only", style={'margin-top': '70px'})
+                            ],)
                 
                     elif pathname == '/student/student_profile':
-                        #return student_profile.layout  # Assuming you have this layout defined
                         returnlayout = student_profile.layout
         
                     elif pathname == '/student/student_profile_edit':
@@ -124,8 +112,6 @@ if __name__ == '__main__':
                     not userid
                 ]
                 sessionlogout = any(logout_conditions)
-                print(sessionlogout)
-                print('--')
                 # hide navbar if logged-out; else, set class/style to default
                 navbar_classname = 'd-none' if sessionlogout else ''
         
@@ -136,4 +122,4 @@ if __name__ == '__main__':
         else:
             raise PreventUpdate
 
-    app.run_server(debug=True)  # Set debug=True for development
+    app.run_server(debug=False)  
